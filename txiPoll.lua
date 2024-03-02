517b8e53a0340a2f913febdf5e1c138c77a0f6d3
Evan Johnson <ev.jonnson@gmail.com>
2024-03-02 12:21:41 -0700

    remove cruft

--- txiPoll.lua
+++ txiPoll.lua
@@ -16,8 +16,6 @@ clock.run( function()
     clock.sleep(0.01) -- set the polling speed
     ii.txi.get('param', n)
     ii.txi.get('in', n)
-    -- print("param "..n.."= "..txiVals.param[n])
-    -- print("cv "..n.."= "..txiVals.cv[n])
     n = (n % 4) + 1
   end
 end)
