de2d63a35a86dbe59129f1f2900cd6484dbf523d
Evan Johnson <ev.jonnson@gmail.com>
2024-03-03 02:26:45 -0700

    envelope triggering working

--- dual-ar-envelopes.lua
+++ dual-ar-envelopes.lua
@@ -23,44 +23,57 @@ parameters = {
 function init()
     input[1].mode('change', 1.0, 0.1, 'rising')
     input[2].mode('change', 1.0, 0.1, 'rising')
+
+    output[4].volts = .35
+
+    currentMode = 'primary'
+    primaryUpdateLoop = clock.run(parameterUpdater,'primary')
 end
 
 input[1].change = function(state)
-    --[[
-    print('length1: ' ..length)
-    print('attack1: ' ..attack)
-    print('release1: ' ..release)
---]]
-    output[1].action = ar(attack1, release1, 8, 'linear')
+    local len = (parameters.len1 + parameters.lenOffset1) * parameters.lenRange1
+    local attack = len * parameters.ratio1
+    local release = len * (1 - parameters.ratio1)
+
+    output[1].action = ar(attack, release, 8, parameters.shape1)
     output[1](s)
 end
 
 input[2].change = function(state)
-    ii.txi.get('param', 3)
-    ii.txi.get('param', 4)
-    length2 = txiVals.param
-    --[[
-    print('length2: ' ..length)
-    print('attack2: ' ..attack)
-    print('release2: ' ..release)
---]]
-    output[2].action = ar(attack2, release2, 8, 'linear')
+    local len = (parameters.len2 + parameters.lenOffset2) * parameters.lenRange2
+    local attack = len * parameters.ratio2
+    local release = len * (1 - parameters.ratio1)
+
+    output[2].action = ar(attack, release, 8, parameters.shape2)
     output[2](s)
 end
 
-ii.txi.event = function(e, val)
-    if e.arg == 1 then
-        length1 = val * .4 + .01
-    elseif e.arg == 2 then
-        attack1 = val / 10 * length1
-        release1 = length1 - attack1
-    elseif e.arg == 3 then
-        length2 = val * .4 + .01
-    elseif e.arg == 4 then
-        attack2 = val / 10 * length2
-        release2 = length2 - attack2
+-- mode change code
+function setMode(mode)
+    if mode == 'primary' and currentMode ~= 'primary' then
+        clock.cancel(secondaryUpdateLoop)
+        primaryUpdateLoop = clock.run(parameterUpdater,'primary')
+        currentMode = 'primary'
+    elseif mode == 'secondary' and currentMode ~= 'secondary' then
+        clock.cancel(primaryUpdateLoop)
+        secondaryUpdateLoop = clock.run(parameterUpdater,'secondary')
+        currentMode = 'secondary'
     end
 end
+
+clock.run(function()
+    local prev = math.floor(input[2].volts * 100)
+
+    while true do
+        clock.sleep(0.1)
+        local current = math.floor(input[2].volts * 100)
+        if (prev >= 34 and prev <= 36 and
+                current >= 34 and current <= 36) then
+            setMode('secondary')
+        end
+        prev = current
+    end
+end)
 
 -- txi polling
 txiVals = {
@@ -85,71 +98,112 @@ clock.run(function()
     end
 end)
 
--- translate txi input values based on the mode
-txiProcessors = {
-    basic = function()
-        local prevs = {
-            param = {},
-            cv = {}
-        }
-        local handlers = {
-            param = {
-                'len1','ratio1','len2','ratio2'
-                -- [1] = function(val)
-                --     parameters.len1 = val / 10
-                -- end,
-                -- [2] = function(val)
-                --     parameters.ratio1 = val / 10
-                -- end
-                -- [3] = function(val)
-                --     parameters.len2 = val / 10
-                -- end,
-                -- [4] = function(val)
-                --     parameters.ratio2 = val / 10
-                -- end
-            },
-            cv = {
-                'lenOffset1','ratioOffset1','lenOffset2','ratioOffset2'
-                -- [1] = function(val)
-                --     parameters.lenOffset1 = val / 10
-                -- end,
-                -- [2] = function(val)
-                --     parameters.ratioOffset1 = val / 10
-                -- end
-                -- [3] = function(val)
-                --     parameters.lenOffset2 = val / 10
-                -- end,
-                -- [4] = function(val)
-                --     parameters.ratioOffset2 = val / 10
-                -- end
-            }
-        }
-        local n = 1
-        while true do
-            clock.sleep(0.01)
-
-            local currentParamVal = txiVals.param[n]
-            local currentCvVal = txiVals.cv[n]
-            local prevParamVal = prevs.param[n]
-            local prevCvVal = prevs.cv[n]
-
-            if not prevParamVal then -- initialize
-                prevs.param[n] = currentParamVal
-            elseif currentParamVal ~= prevParamVal then
-                prevs.param[n] = currentCvVal
-                parameters[handlers.param[n]] = currentParamVal / 10 -- param is 0-10, we want percentage
+local handlers = {
+    primary = {
+        param = {
+            [1] = function(val)
+                parameters.len1 = val / 10
+            end,
+            [2] = function(val)
+                parameters.ratio1 = val / 10
+            end,
+            [3] = function(val)
+                parameters.len2 = val / 10
+            end,
+            [4] = function(val)
+                parameters.ratio2 = val / 10
             end
-
-            if not prevCvVal then
-                prevs.cv[n] = currentCvVal
-            elseif currentCvVal ~= prevCvVal then
-                prevs.cv[n] = currentCvVal
-                parameters[handlers.cv[n]] = currentCvVal / 5 -- cv range -5v - 5v
+        },
+        cv = {
+            [1] = function(val)
+                parameters.lenOffset1 = val / 5
+            end,
+            [2] = function(val)
+                parameters.ratioOffset1 = val / 5
+            end,
+            [3] = function(val)
+                parameters.lenOffset2 = val / 5
+            end,
+            [4] = function(val)
+                parameters.ratioOffset2 = val / 5
             end
-            n = (n % 4) + 1
+        }
+    },
+    secondary = {
+        param = {
+            [1] = function(val)
+                parameters.lenRange1 = 1 + val
+                parameters.lenRange2 = 1 + val
+            end,
+            [2] = function(val)
+                if val < 2 then
+                    parameters.shape1 = 'log'
+                    parameters.shape2 = 'log'
+                elseif val > 8 then
+                    parameters.shape1 = 'exp'
+                    parameters.shape2 = 'exp'
+                else
+                    parameters.shape1 = 'linear'
+                    parameters.shape2 = 'linear'
+                end
+            end,
+            [3] = function(val)
+                parameters.lenRange2 = 1 + val
+            end,
+            [4] = function(val)
+                if val < 2 then
+                    parameters.shape2 = 'log'
+                elseif val > 8 then
+                    parameters.shape2 = 'exp'
+                else
+                    parameters.shape2 = 'linear'
+                end
+            end
+        },
+        cv = {
+            [1] = function(val)
+                parameters.lenOffset1 = val / 5
+            end,
+            [2] = function(val)
+                parameters.ratioOffset1 = val / 5
+            end,
+            [3] = function(val)
+                parameters.lenOffset2 = val / 5
+            end,
+            [4] = function(val)
+                parameters.ratioOffset2 = val / 5
+            end
+        }
+    }}
+
+function parameterUpdater(mode)
+    local prevs = {
+        param = {},
+        cv = {}
+    }
+    local n = 1
+    while true do
+        clock.sleep(.01)
+
+        local currentParamVal = txiVals.param[n]
+        local currentCvVal = txiVals.cv[n]
+        local prevParamVal = prevs.param[n]
+        local prevCvVal = prevs.cv[n]
+
+        if not prevParamVal then -- initialize
+            prevs.param[n] = currentParamVal
+        elseif math.abs(currentParamVal - prevParamVal) > 0.005 then
+            print('detected param '..n..' change')
+            handlers[mode].param[n](currentParamVal)
+            prevs.param[n] = currentParamVal
         end
+
+        if not prevCvVal then
+            prevs.cv[n] = currentCvVal
+        elseif math.abs(currentCvVal - prevCvVal) > 0.005 then
+            handlers[mode].cv[n](currentCvVal)
+            prevs.cv[n] = currentCvVal
+        end
+        n = (n % 4) + 1
     end
-}
-
-basicUpdateLoop = clock.run(txiProcessors.basic)
-
+end
